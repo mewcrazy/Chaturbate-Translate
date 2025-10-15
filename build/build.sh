78f258d2c5b00fb0394d02662a1a8e63ea1aa91b
Dennis Bitsch <kontakt@dennis-bitsch.de>
2025-10-15 05:39:14 +0200

    Fix Deployment & Release Tag

--- build/build.sh
+++ build/build.sh
@@ -12,7 +12,7 @@ ln -s ../../../../node_modules/jquery/dist/jquery.min.js extensions/chrome/js/jq
 /usr/local/bin/crx3 extensions/chrome -o extensions/chrome.crx -p /home/githubrunner/extension_keys/chrome.pem
 
 # Build Firefox Extension File
-# /usr/local/bin/web-ext build -o -s extensions/chrome -a ./extensions -n firefox.zip
+/usr/local/bin/web-ext build -o -s extensions/chrome -a ./extensions -n firefox.zip
 
 # Build Opera Extension File
-# /usr/local/bin/crx3 extensions/chrome -o extensions/opera.crx -p /home/githubrunner/extension_keys/opera.pem
+/usr/local/bin/crx3 extensions/chrome -o extensions/opera.crx -p /home/githubrunner/extension_keys/opera.pem
